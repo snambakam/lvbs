43d381794c2a61e4a8925b1c9ad9f70332a4e320
Sriram Nambakam <snambakam@gmail.com>
2026-03-23 16:36:08 -0700

    add some convenience scripts to chunk large git repos

---
+++ support/developer/scripts/configure-git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git config pack.packSizeLimit 2g
+git config pack.windowMemory 1g
+git config pack.packThreads 8
---
+++ support/developer/scripts/git-repack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git repack -ad --depth=50 --window=50 --max-pack-size=2g
+
