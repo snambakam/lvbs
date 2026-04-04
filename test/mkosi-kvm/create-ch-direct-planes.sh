fc1ad548ef0c2fe983bd1ab4ee25da3773c57bda
Sriram Nambakam <snambakam@microsoft.com>
2026-04-03 17:14:43 -0700

    Add create-ch-direct-planes.sh for testing KVM VM planes

---
+++ test/mkosi-kvm/create-ch-direct-planes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cloud-hypervisor \
+    -v \
+    --log-file /tmp/ch-debug.log \
+    --kernel fedora-kvm.vmlinuz \
+    --initramfs fedora-kvm.initrd \
+    --cmdline "root=/dev/vda2 console=ttyS0 printk.caller=1" \
+    --disk path=fedora-kvm.raw \
+    --cpus boot=2 \
+    --memory size=1G \
+    --serial file=/tmp/plane-serial.log \
+    --plane "plane_id=1,boot_vcpus=2,max_vcpus=2,kernel=fedora-kvm.vmlinuz,initramfs=fedora-kvm.initrd,cmdline=root=/dev/vda2 console=ttyS0 printk.caller=1"
+
