5d1a806b454ed7d7276e91c7800f73fdce0bdf48
Sriram Nambakam <snambakam@snambakam-dev-5.redmond.corp.microsoft.com>
2026-04-15 10:19:22 -0700

    Use script to build and install the kernel

--- test/kernel/build.sh
+++ test/kernel/build.sh
@@ -3,13 +3,44 @@
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . "$SCRIPTS_DIR/common.sh"
 
+
+build() {
+	if [ ! -f "$BUILD_ROOT/.config" ]; then
+		cp $LINUX_KERNEL_CONFIG "$BUILD_ROOT/.config"
+	fi
+
+	make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" olddefconfig
+	make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" -j$(nproc)
+	make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" -j$(nproc) modules
+}
+
+install() {
+	sudo make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" install
+	sudo make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" install_modules
+}
+
+#
+# Main
+#
+
 BUILD_ROOT="$SCRIPTS_DIR/build"
 mkdir -p "$BUILD_ROOT"
 
-if [ ! -f "$BUILD_ROOT/.config" ]; then
-	cp $LINUX_KERNEL_CONFIG "$BUILD_ROOT/.config"
+CMD="build"
+if [ $# -gt 0 ]; then
+	CMD="$1"
 fi
 
-make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" olddefconfig
-make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" -j$(nproc)
-make -C $LINUX_SRC_ROOT O="$BUILD_ROOT" -j$(nproc) modules
+case "$CMD" in
+	"build")
+		build
+		;;
+	"install")
+		install
+		;;
+	*)
+		echo "Error: unrecognized command - $CMD"
+		exit 1
+		;;
+esac
+
